5a9e2d0bd1fb55d0d433323040c7da216ee67d46
Arthur8777 <arthurmurillo87@gmail.com>
2025-09-03 22:24:05 -0300

    Implementei o main com as opcoes

--- src/ucb/aplicacao/cli/Main.java
+++ src/ucb/aplicacao/cli/Main.java
@@ -1,15 +1,61 @@
 package ucb.aplicacao.cli;
 
+import ucb.aplicacao.model.Tarefa;
+import ucb.aplicacao.service.TarefaServico;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        TarefaServico servico = new TarefaServico();
+
+        while (true) {
+            System.out.println("\n\n===== Gerenciador de Tarefas =====");
+            System.out.println("1. Criar Tarefa");
+            System.out.println("2. Listar Tarefas");
+            System.out.println("3. Atualizar Tarefa");
+            System.out.println("4. Remover Tarefa");
+            System.out.println("5. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); // Consumir quebra de linha
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Titulo: ");
+                    String titulo = scanner.nextLine();
+                    System.out.print("Descrição: ");
+                    String descricao = scanner.nextLine();
+                    Tarefa novaTarefa = servico.criarTarefa(titulo, descricao);
+                    System.out.print("Tarefa criada com sucesso!: " + novaTarefa.getTitulo());
+                    break;
+                case 2:
+                    System.out.println("\n--- Lista Tarefas ---");
+                    List<Tarefa> tarefas = servico.listarTarefas();
+                    for (int i = 0; i < tarefas.toArray().length; i++) {
+                        System.out.println("\nTarefa " + (i+1) + ":");
+                        System.out.println("Id = " + tarefas.get(i).getId());
+                        System.out.println("Titulo = " + tarefas.get(i).getTitulo());
+                        System.out.println("Descrição = " + tarefas.get(i).getDescricao());
+                        System.out.println("Completa = " + tarefas.get(i).isCompleta());
+                        System.out.println("Data criação = " + tarefas.get(i).getDataCriacao());
+                    }
+                    break;
+                case 3:
+                    System.out.println("Opção escolhida: Atualizar Tarefa");
+                    break;
+                case 4:
+                    System.out.println("Opção escolhida: Remover Tarefa");
+                    break;
+                case 5:
+                    System.out.println("Opção escolhida: Sair");
+                    return;
+            }
         }
+
     }
 }
